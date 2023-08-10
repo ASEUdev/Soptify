712ed6ba44b172dffd5fa7abe795e286bb555120
ASEUdev <88355964+ASEUdev@users.noreply.github.com>
2023-08-10 18:05:15 -0500

    Add files via upload

--- relaxis/index.php
+++ relaxis/index.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+if (!isset($_SESSION["user_id"])) {
+	// Redirigir al formulario de inicio de sesión si el usuario no ha iniciado sesión
+	header("Location: ../");
+	exit();
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,6 +19,7 @@
 	<link rel='stylesheet'
 		href='https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&amp;display=swap'>
 	<link rel="stylesheet" href="dist/style.css">
+	<link rel="stylesheet" href="src/style.css">
 </head>
 
 <body>
@@ -18,12 +28,16 @@
 		<aside id="sidebar" class="sidebar break-point-sm has-bg-image">
 			<a id="btn-collapse" class="sidebar-collapser"><i class="ri-arrow-left-s-line"></i></a>
 			<div class="image-wrapper">
-				<img src="assets/images/sidebar-bg.jpg" alt="sidebar background" />
+				<!-- <img src="assets/images/sidebar-bg.jpg" alt="sidebar background" /> -->
 			</div>
 			<div class="sidebar-layout">
 				<div class="sidebar-header">
 					<div class="pro-sidebar-logo">
-						<div>R</div>
+						<div>
+							<a href="#" onclick="nothingHere(); return false;">
+								R
+							</a>
+						</div>
 						<h5>Relaxis Song</h5>
 					</div>
 				</div>
@@ -53,98 +67,22 @@
 							<li class="menu-item">
 								<a href="#">
 									<span class="menu-icon">
-										<i class="ri-file-text-line"></i>
-									</span>
-									<span class="menu-title">Muro</span>
-								</a>
-							</li>
-							<li class="menu-item">
-								<a href="#">
-									<span class="menu-icon">
 										<i class="ri-account-circle-line"></i>
 									</span>
 									<span class="menu-title">Perfil</span>
 								</a>
 							</li>
-							<!-- MENU DE TEMA DE LA PAGINA  -->
-							<!-- <li class="menu-item sub-menu">
-								<a href="#">
-									<span class="menu-icon">
-										<i class="ri-paint-brush-fill"></i>
-									</span>
-									<span class="menu-title">Theme</span>
-								</a>
-								<div class="sub-menu-list">
-									<ul>
-										<li class="menu-item">
-											<a href="#">
-												<span class="menu-title">Dark</span>
-											</a>
-										</li>
-										<li class="menu-item">
-											<a href="#">
-												<span class="menu-title">Light</span>
-											</a>
-										</li>
-									</ul>
-								</div>
-							</li> -->
-
-							<!-- SECCION CON CONTENIDO EXTRA -->
-							<!-- <li class="menu-header" style="padding-top: 20px"><span> EXTRA </span></li>
 							<li class="menu-item">
 								<a href="#">
 									<span class="menu-icon">
-										<i class="ri-book-2-fill"></i>
+										<i class="ri-file-text-line"></i>
 									</span>
-									<span class="menu-title">Documentation</span>
-									<span class="menu-suffix">
-										<span class="badge secondary">Beta</span>
-									</span>
+									<span class="menu-title">Muro</span>
 								</a>
 							</li>
-							<li class="menu-item">
-								<a href="#">
-									<span class="menu-icon">
-										<i class="ri-calendar-fill"></i>
-									</span>
-									<span class="menu-title">Calendar</span>
-								</a>
-							</li>
-							<li class="menu-item">
-								<a href="#">
-									<span class="menu-icon">
-										<i class="ri-service-fill"></i>
-									</span>
-									<span class="menu-title">Examples</span>
-								</a>
-							</li> -->
 						</ul>
 					</nav>
 				</div>
-				<!-- <div class="sidebar-footer">
-					<div class="footer-box">
-						<div>
-							<img class="react-logo"
-								src="https://user-images.githubusercontent.com/25878302/213938106-ca8f0485-3f30-4861-9188-2920ed7ab284.png"
-								alt="react" />
-						</div>
-						<div style="padding: 0 10px">
-							<span style="display: block; margin-bottom: 10px">Lorem ipsum dolor sit amet, consectetur
-								adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-								Consectetur purus ut faucibus pulvinar.
-							</span>
-							<div style="margin-bottom: 15px">
-								<img alt="preview badge"
-									src="https://img.shields.io/github/stars/azouaoui-med/react-pro-sidebar?style=social" />
-							</div>
-							<div>
-								<a href="https://github.com/azouaoui-med/react-pro-sidebar" target="_blank">Check it
-									out!</a>
-							</div>
-						</div>
-					</div>
-				</div> -->
 			</div>
 		</aside>
 		<div id="overlay" class="overlay"></div>
@@ -154,94 +92,22 @@
 					<a id="btn-toggle" href="#" class="sidebar-toggler break-point-sm">
 						<i class="ri-menu-line ri-xl"></i>
 					</a>
-					<h1 style="margin-bottom: 0">Titulo</h1>
-					<span style="display: inline-block">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-						labore et dolore magna aliqua. Consectetur purus ut faucibus pulvinar. Ipsum consequat nisl vel
-						pretium lectus quam id leo in. Lacus vel facilisis volutpat est velit egestas. Sit amet nisl
-						purus in. Id neque aliquam vestibulum morbi. Eleifend mi in nulla posuere sollicitudin. Quis
-						imperdiet massa tincidunt nunc pulvinar sapien et. Tortor aliquam nulla facilisi cras fermentum
-						odio eu. Id neque aliquam vestibulum morbi blandit cursus risus. Faucibus ornare suspendisse sed
-						nisi lacus sed viverra tellus. Tincidunt eget nullam non nisi est sit amet facilisis.
-					</span>
-					<br />
-					<span>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-						labore et dolore magna aliqua. Consectetur purus ut faucibus pulvinar. Ipsum consequat nisl vel
-						pretium lectus quam id leo in. Lacus vel facilisis volutpat est velit egestas. Sit amet nisl
-						purus in. Id neque aliquam vestibulum morbi. Eleifend mi in nulla posuere sollicitudin. Quis
-						imperdiet massa tincidunt nunc pulvinar sapien et. Tortor aliquam nulla facilisi cras fermentum
-						odio eu. Id neque aliquam vestibulum morbi blandit cursus risus. Faucibus ornare suspendisse sed
-						nisi lacus sed viverra tellus. Tincidunt eget nullam non nisi est sit amet facilisis
-					</span>
-					<!-- <div style="margin-top: 10px">
-						<a href="https://github.com/azouaoui-med/pro-sidebar-template" target="_blank">
-							<img alt="GitHub stars"
-								src="https://img.shields.io/github/stars/azouaoui-med/pro-sidebar-template?style=social" />
-						</a>
-						<a href="https://github.com/azouaoui-med/pro-sidebar-template" target="_blank">
-							<img alt="GitHub forks"
-								src="https://img.shields.io/github/forks/azouaoui-med/pro-sidebar-template?style=social" />
-						</a>
-					</div> -->
+					<div class="content">
+						<!-- CODIGO DEL AJAX -->
+						<h1>Bienvenido a la página de inicio</h1>
+						<p>Contenido de la página de inicio...</p>
+					</div>
 				</div>
-				<!-- <div>
-					<h2>Features</h2>
-					<ul>
-						<li>Fully responsive</li>
-						<li>Collapsable sidebar</li>
-						<li>Multi level menu</li>
-						<li>RTL support</li>
-						<li>Customizable</li>
-					</ul>
-				</div> -->
-				<!-- <div>
-					<h2>Resources</h2>
-					<ul>
-						<li>
-							<a target="_blank" href="https://github.com/azouaoui-med/css-pro-layout">
-								Css Pro Layout</a>
-						</li>
-						<li>
-							<a target="_blank" href="https://github.com/popperjs/popper-core"> Popper Core</a>
-						</li>
-						<li>
-							<a target="_blank" href="https://remixicon.com/"> Remix Icons</a>
-						</li>
-					</ul>
-				</div> -->
-
-				<!-- PIECERA CON INFORMACION DE CONTACTO/ REDES SOCIALES -->
-				<footer class="footer">
-					<!-- <small style="margin-bottom: 20px; display: inline-block">
-						© 2023 made with
-						<span style="color: red; font-size: 18px">&#10084;</span> by -
-						<a target="_blank" href="https://azouaoui.netlify.com"> Mohamed Azouaoui </a>
-					</small>
-					<br />
-					<div class="social-links">
-						<a href="https://github.com/azouaoui-med" target="_blank">
-							<i class="ri-github-fill ri-xl"></i>
-						</a>
-						<a href="https://twitter.com/azouaoui_med" target="_blank">
-							<i class="ri-twitter-fill ri-xl"></i>
-						</a>
-						<a href="https://codepen.io/azouaoui-med" target="_blank">
-							<i class="ri-codepen-fill ri-xl"></i>
-						</a>
-						<a href="https://www.linkedin.com/in/mohamed-azouaoui/" target="_blank">
-							<i class="ri-linkedin-box-fill ri-xl"></i>
-						</a>
-					</div> -->
-				</footer>
 			</main>
 			<div class="overlay"></div>
 		</div>
 	</div>
 	<!-- partial -->
 	<script src='https://unpkg.com/@popperjs/core@2'></script>
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 	<script src="dist/script.js"></script>
-
+	<script src="src/normalCode.js"></script>
+	<script src="src/content.js"></script>
 </body>
 
 </html>
